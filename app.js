f997cef68e8cf4e5d0a6a625c749b6c2dc7ca8ca
abrartariqulislam <islamtariqul010@gmail.com>
2023-08-15 16:45:40 +0600

    finished digital clock

--- app.js
+++ app.js
@@ -13,3 +13,34 @@ setInterval(() => {
   minutes.style.transform = `rotateZ(${minute}deg)`;
   seconds.style.transform = `rotateZ(${second}deg)`;
 });
+
+// digital clock
+const digitalHours = document.querySelector(".time_hours");
+const digitalMinutes = document.querySelector(".time_minutes");
+const digitalSeconds = document.querySelector(".time_seconds");
+let digitalDayNight = document.querySelector(".time_dayNight");
+
+setInterval(() => {
+  const digitalTime = new Date();
+
+  let digitalHour = digitalTime.getHours();
+  let digitalMinute = digitalTime.getMinutes();
+  let digitalSecond = digitalTime.getSeconds();
+
+  if(digitalHour >12){
+    digitalHour = digitalHour - 12
+    digitalDayNight.textContent = "PM"
+  }
+  if(digitalHour < 10){
+    digitalHour = "0" + digitalHour
+  }
+  if(digitalMinute < 10){
+    digitalMinute = "0" + digitalMinute
+  }
+  if(digitalSecond < 10){
+    digitalSecond = "0" + digitalSecond
+  }
+  digitalHours.textContent = digitalHour;
+  digitalMinutes.textContent = digitalMinute;
+  digitalSeconds.textContent = digitalSecond;
+});
